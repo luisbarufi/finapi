db6eb334c3e5b3a0957b4631c69d09079c4ee669
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-04-27 16:24:22 -0300

    creating account withdrawal

--- src/index.js
+++ src/index.js
@@ -20,6 +20,18 @@ function verifyIfExistsAccount(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((accumulator, operation) => {
+    if (operation.type === 'credit') {
+      return accumulator + operation.amount;
+    } else {
+      return accumulator - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
   const customersAlreadyExist = customers.some(
@@ -56,6 +68,28 @@ app.post('/deposit', verifyIfExistsAccount, (request, response) => {
     amount,
     created_at: new Date(),
     type: "credit"
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccount, (request, response) => {
+  const { amount } = request.body;
+
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: 'Insufficient funds!'})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
   };
 
   customer.statement.push(statementOperation);
